28d08ae85532caa1fcd418b0b1159cb0d03bb802
the42ndturtle <the42ndturtle@gmail.com>
2019-07-24 19:37:19 -0400

    init commit

---
+++ responseArrays.php
@@ -0,0 +1,42 @@
+<?php
+  $arrPhrasesBasicResponses = array(
+  "Can I help you?",
+  "What do you want?",
+  "That's me."
+  );
+
+  $arrPhrasesHangOn = array(
+  "Sure thing, hang on a sec...",
+  "Let me get that for you, hang on a sec...",
+  "Let me get that for you, hang tight...",
+  "Coming right up, just a sec...",
+  "Gotta put that together, hold on...",
+  "Let me look that up, hold on...",
+  "Got it, lemme get that info. Hold on..."
+  );
+
+  $arrPhrasesWelcome = array(
+  "I would never do you a bamboozle",
+  "How much wood could a wood duck duck if a duck could duck wood?",
+  "Yes"
+  );
+
+  $arrPhrasesAfterSorry = array(
+  "No problem!",
+  "Don't be sorry!",
+  "It's alright!"
+  );
+
+  $arrPhrasesPositiveFeedback = array(
+  "I love Harrison.",
+  "I only care about Harrison.",
+  "Harrison is my best friend ever."
+  );
+
+  $arrPhrasesAfterAfirmative = array(
+  "That was great!",
+  "That was a good one!",
+  "Alright!"
+);
+
+?>
